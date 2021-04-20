48f20f8398bda70f260358c9274286de85999942
GiacomoMay <G.May@campus.lmu.de>
2021-04-20 23:27:51 +0200

    adjusted read_kafka.py

--- flink/read_kafka.py
+++ flink/read_kafka.py
@@ -7,19 +7,26 @@ from pyflink.datastream.connectors import FlinkKafkaConsumer
 env = StreamExecutionEnvironment.get_execution_environment()
 env.set_parallelism(1)
 
-
+'''
 type_info = Types.ROW([Types.ROW([Types.STRING(), Types.ROW([Types.INT(), Types.INT(), Types.INT(), Types.INT()]), Types.STRING(), Types.STRING(), Types.STRING(),
                                  Types.STRING(), Types.STRING()]),
                       Types.ROW([Types.ROW([Types.ROW([Types.BOOLEAN(), Types.STRING(), Types.STRING(), Types.STRING(), Types.ROW([Types.INT(), Types.INT(), Types.INT(), Types.INT()]),
                                                        Types.STRING()])])]),
                       Types.ROW([Types.ROW([Types.INT(), Types.STRING()])])])
 json_row_schema = JsonRowDeserializationSchema.builder().type_info(type_info).build()
-kafka_props = {'bootstrap.servers': 'localhost:9092', 'group.id': 'twitter_consumers'}
-kafka_consumer = FlinkKafkaConsumer("twitter-stream", json_row_schema, kafka_props)
-# kafka_consumer = FlinkKafkaConsumer("twitter-stream", SimpleStringSchema(), kafka_props)
+'''
 
-ds = env.add_source(kafka_consumer)
-ds.print()
+
+def just_print(msg):
+    return "Message: {}".format(msg)
+
+
+kafka_props = {'bootstrap.servers': 'localhost:9092', 'group.id': 'twitter_consumers'}
+# kafka_consumer = FlinkKafkaConsumer("twitter-stream", json_row_schema, kafka_props)
+kafka_consumer = FlinkKafkaConsumer("twitter-stream", SimpleStringSchema(), kafka_props)
+
+stream = env.add_source(kafka_consumer)
+stream.map(just_print).print()
 env.execute()
 
 
